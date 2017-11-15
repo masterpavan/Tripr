37eaaed0ff594c626900314ed586e47ea2b203b5
Pavan Purewal <pavanpurewal0@gmail.com>
2017-11-14 19:49:21 -0800

    attempt AsyncStorage implementation

--- App/TripStackNav/TripListView/TripListView.js
+++ App/TripStackNav/TripListView/TripListView.js
@@ -1,50 +1,56 @@
 
 import React, {Component} from "react";
-import {Button, Image, ScrollView, Text, View} from 'react-native';
+import {Button, Image, ScrollView, Text, View, AsyncStorage} from 'react-native';
 import ButtonRectangle from './ButtonRectangle'
 
 import styles from './Styles/ChooseCityPlannerStyles'
 
 export default class TripListView extends React.Component {
 
-
+    componentDidMount() {
+        AsyncStorage.setItem("currentTrips", {
+            tripToItaly: {
+                name: "Trip to Italy"
+            },
+            tripToEngland: {
+                name: "Trip to England"
+            },
+            tripToRussia: {
+                name: "Trip to Russia"
+            }
+        });
+        AsyncStorage.getItem("currentTrips").then((value) => {
+            this.setState({"currentTrips": value});
+        }).done();
+    };
 
     static navigationOptions = {
         title: 'TripList'
     };
-    render() {
 
+    generateButton() {
+        this.state.currentTrips.forEach(element => {
+            return (
+                <ButtonRectangle
+                    onPress={() => this.props.navigation.navigate('TripDetailView')}
+                    style={styles.componentButton}
+                    image={require('./icon_food.png')}
+                    text={element.name}
+                />
+            )
+        })
+    }
+
+    render() {
         return (
             <View style={styles.mainContainer}>
                 <Image source={require('./navyblue.png')} style={styles.backgroundImage} resizeMode='stretch' />
                 <Text style={styles.sectionText}>
                     Discover
                 </Text>
-
                 <ScrollView showsVerticalScrollIndicator={false} bounces={false} style={styles.container}>
                     <View style={styles.buttonsContainer}>
-                        <ButtonRectangle onPress={() => this.props.navigation.navigate('TripDetailView')} style={styles.componentButton} image={require('./icon_food.png')} text='Trip To Italy' />
-                    </View>
-                    <View style={styles.buttonsContainer}>
-                        <ButtonRectangle onPress={() => this.props.navigation.navigate('TripDetailView')} style={styles.usageButton} image={require('./icon_food.png')} text='Trip to England' />
-                    </View>
-                    <View style={styles.buttonsContainer}>
-                        <ButtonRectangle onPress={() => this.props.navigation.navigate('TripDetailView')} style={styles.apiButton} image={require('./icon_food.png')} text='Trip to America' />
-                    </View>
-                    <View style={styles.buttonsContainer}>
-                        <ButtonRectangle onPress={() => this.props.navigation.navigate('TripDetailView')} style={styles.deviceButton} image={require('./icon_food.png')} text='Trip to Russia' />
-                    </View>
-                    <View style={styles.buttonsContainer}>
-                        <ButtonRectangle onPress={() => this.props.navigation.navigate('TripDetailView')} style={styles.deviceButton} image={require('./icon_food.png')} text='Trip to Russia' />
-                    </View>
-                    <View style={styles.buttonsContainer}>
-                        <ButtonRectangle onPress={() => this.props.navigation.navigate('TripDetailView')} style={styles.deviceButton} image={require('./icon_food.png')} text='Trip to Russia' />
-                    </View>
-                    <View style={styles.buttonsContainer}>
-                        <ButtonRectangle onPress={() => this.props.navigation.navigate('TripDetailView')} style={styles.deviceButton} image={require('./icon_food.png')} text='Trip to Russia' />
-                    </View>
-                    <View style={styles.buttonsContainer}>
-                        <ButtonRectangle onPress={() => this.props.navigation.navigate('TripDetailView')} style={styles.deviceButton} image={require('./icon_food.png')} text='Trip to Russia' />
+                        {this.generateButton()}
                     </View>
                 </ScrollView>
             </View>
